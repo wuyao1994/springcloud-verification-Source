2ad27d54f67e01d3d48dd5aeba6abc7d0831d4ed
wuyao1994 <wuyaoiot@163.com>
2018-06-24 16:48:07 +0800

    change hosts

--- test.sh
+++ test.sh
@@ -23,10 +23,10 @@ ${PWD}/$2/build/libs/;
 }
 
 # Export the active docker machine IP
-export DOCKER_IP=$(docker-machine ip $(docker-machine active))
+export DOCKER_IP=192.168.0.114
 
 # docker-machine doesn't exist in Linux, assign default ip if it's not set
-DOCKER_IP=${DOCKER_IP:-0.0.0.0}
+DOCKER_IP=192.168.0.114
 
 # Remove existing containers
 docker-compose stop
